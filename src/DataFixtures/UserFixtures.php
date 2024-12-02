f0547bf7c6824b457dd9a07d41c4dfd24861176e
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-12-02 15:25:28 +0100

    feat: create the page to add a scenario

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+final class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 5; ++$i) {
+            $user = new User();
+            $user->setEmail($faker->email());
+            $user->setPassword($faker->password());
+            $user->setPseudo($faker->userName());
+            $user->setVerified(true);
+
+            $manager->persist($user);
+
+            $this->addReference('user_' . $i, $user);
+        }
+
+        $manager->flush();
+    }
+}
