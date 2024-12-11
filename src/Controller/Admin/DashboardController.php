30a12d43de25daef9099cce87224f862be90e0f0
thibauttaglialatela <thibauttaglialatela@gmail.com>
2024-12-11 16:45:21 +0100

    fix: bug with EasyAdmin and Twig

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -16,13 +16,23 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
+    public function __construct(private ChartBuilderInterface $chartBuilder)
+    {
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        return $this->render('admin/index.html.twig');
+        $chart = $this->chartBuilder->createChart(Chart::TYPE_BAR);
+
+        return $this->render('admin/index.html.twig', [
+            'chart' => $chart,
+        ]);
     }
 
     public function configureDashboard(): Dashboard
