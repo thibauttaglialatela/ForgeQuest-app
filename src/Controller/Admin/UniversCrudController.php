4d8bb14eb52b5eb6bb6487c6098828deb25f5f17
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-01-16 16:48:32 +0100

    fix: translate titles of the AdminCrudControllers

--- src/Controller/Admin/UniversCrudController.php
+++ src/Controller/Admin/UniversCrudController.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace App\Controller\Admin;
 
 use App\Entity\Univers;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -17,6 +20,14 @@ class UniversCrudController extends AbstractCrudController
     public static function getEntityFqcn(): string
     {
         return Univers::class;
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->update(Crud::PAGE_INDEX, Action::NEW,
+                fn (Action $action) => $action->setLabel('Ajouter un univers'))
+        ;
     }
 
     public function configureFields(string $pageName): iterable
@@ -33,5 +44,15 @@ class UniversCrudController extends AbstractCrudController
             ->setUploadedFileNamePattern('[name]-[randomhash].[extension]');
         yield TextField::new('imageAlt', 'texte alternatif')->onlyOnForms();
         yield DateTimeField::new('createdAt')->onlyOnDetail();
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->renderContentMaximized()
+            ->setPageTitle('index', 'Liste des univers de jeu')
+            ->setPageTitle('detail', 'Un univers')
+            ->setPageTitle('edit', 'Modifier un univers')
+        ;
     }
 }
