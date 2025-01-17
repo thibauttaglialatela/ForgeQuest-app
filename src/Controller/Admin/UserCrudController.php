4c5b9d60b52e6f2893fe703a5fc6c2e01683ae08
thibauttaglialatela <thibauttaglialatela@gmail.com>
2025-01-17 16:43:55 +0100

    feat: create user from admin
    an admin can create a user from the admin panel

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -8,7 +8,10 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
@@ -16,9 +19,20 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 class UserCrudController extends AbstractCrudController
 {
+    public function __construct(public UserPasswordHasherInterface $passwordHasher)
+    {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -37,7 +51,20 @@ class UserCrudController extends AbstractCrudController
         yield IdField::new('id', 'ID')->onlyOnIndex();
         yield EmailField::new('email', 'Email');
         yield TextField::new('pseudo', 'Pseudo');
-        yield TextField::new('password', 'Mot de passe')->onlyOnForms()->hideWhenUpdating();
+        yield TextField::new('password', 'Mot de passe')
+            ->onlyOnForms()
+            ->hideWhenUpdating()
+            ->setFormType(RepeatedType::class)
+            ->setFormTypeOptions([
+                'type'           => PasswordType::class,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation Mot de passe'],
+                'mapped'         => false,
+            ]
+            )
+            ->setRequired(Crud::PAGE_NEW === $pageName)
+        ;
+
         yield ArrayField::new('roles', 'Roles')->onlyOnIndex();
         yield BooleanField::new('isVerified', 'Utilisateur vérifié');
         yield CollectionField::new('scenarios')
@@ -55,5 +82,47 @@ class UserCrudController extends AbstractCrudController
             ->setPageTitle('detail', 'Fiche d\'utilisateur')
             ->setPageTitle('edit', 'Modifier un utilisateur')
         ;
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface
+    {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+
+        return $this->addPasswordEventListener($formBuilder);
+    }
+
+    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface
+    {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    }
+
+    private function hashPassword(): callable
+    {
+        return function (FormEvent $event) {
+            $form = $event->getForm();
+            if (!$form->isValid()) {
+                return;
+            }
+
+            $password = $form->get('password')->getData();
+            if (null === $password) {
+                return;
+            }
+
+            $entity = $form->getData();
+            if (!$entity instanceof PasswordAuthenticatedUserInterface) {
+                throw new \LogicException('Dois implémenter PasswordAuthenticatedUserInterface');
+            }
+
+            $passwordHashed = $this->passwordHasher->hashPassword($entity, $password);
+            $form->getData()->setPassword($passwordHashed);
+        };
     }
 }
